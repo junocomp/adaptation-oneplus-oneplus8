030e7abffbe672d1b1f712ddfd968a9befb7c419
Juno Computers <thelookouts@hotmail.com>
2024-02-22 13:52:49 -0500

    Create op8-brightness.sh

---
+++ usr/bin/op8-brightness.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+while true; do
+
+if [ $(wlr-randr | grep -i "enabled" | awk '{print $2}') = 'yes' ]; then
+#	echo "on";
+	cat /sys/class/backlight/panel0-backlight/brightness > ~/.config/backlight-brightness;
+	sleep 1
+
+elif [ $(wlr-randr | grep -i "enabled" | awk '{print $2}') = 'no' ]; then
+#	echo "off";
+	sleep 1
+	cat ~/.config/backlight-brightness > /sys/class/backlight/panel0-backlight/brightness
+fi
+
+done
